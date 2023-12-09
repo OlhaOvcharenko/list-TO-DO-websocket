0a21f421fee0048b12b96f8f5b5b7dd89cfe6a13
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-12-09 18:21:25 +0300

    change server.js

--- server.js
+++ server.js
@@ -11,10 +11,26 @@ const server = app.listen(8000, () => {
 const io = socket(server);
 
 io.on('connection', (socket) => {
-    console.log('New client! Its id – ' + socket.id);
+   
     io.to(socket.id).emit('updateData', tasks);
 
-    socket.on('addTask', () => {});
-    socket.on('removeTask', () => {})
+    socket.on('addTask', (newTask) => {
+      tasks.push(newTask);
+      
+      const message = `New task has been published: ${newTask.task}`;
+  
+      socket.broadcast.emit('addTask', { id: socket.id,  task: message });
+    });
+
+    socket.on('removeTask', () => { 
+    const removedTask = tasks.findIndex(task => task.id === socket.id);
+      if (removedTask !== -1) {
+        const taskName = tasks[removedTask].task; 
+        tasks.splice(removedTask, 1);
+        const message = `${taskName} has been deleted.`;
+        socket.broadcast.emit('removeTask', { id: socket.id,  task: message});
+      }
+    });
+  
 });  
   
